59934afa43619e3c5b91dee21cf3e76edeae87a3
GHOST EXE!? <alessandrooferreira755@gmail.com>
2025-05-29 15:25:55 -0300

    Retirada as chaves de api

--- painel/config/api_config.php
+++ painel/config/api_config.php
@@ -4,8 +4,8 @@ define('API_URL', 'https://api.sandbox.nuvemfiscal.com.br');
 
 // Suas credenciais OAuth 2 - Substitua pelos valores obtidos no Console da Nuvem Fiscal
 <<<<<<< HEAD
-define('NUVEM_FISCAL_API_KEY', 'a0kWBFCvLS22r3uQqyPi'); // Substitua pelo seu Client ID
-define('NUVEM_FISCAL_SECRET_KEY', 'MHiazLWYd3GD4H6ZNloidjQBbk3qK47lpcNPfx2c'); // Substitua pelo seu Client Secret
+define('NUVEM_FISCAL_API_KEY', 'CLIENT_ID'); // Substitua pelo seu Client ID
+define('NUVEM_FISCAL_SECRET_KEY', 'CLIENT_SECRET'); // Substitua pelo seu Client Secret
 
 // Ambiente da Nuvem Fiscal: 'sandbox' ou 'production'
 // Sempre comece com 'sandbox' para testes!
